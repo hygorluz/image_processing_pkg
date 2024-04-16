e4c7d1a3848b4a4c53fdbe1b5d50eb442c187a41
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2024-04-16 16:43:39 -0300

    Finished base logic implementation:
    - Parameters
    - Service/Client
    - Publisher

--- src/image_crop_client_node.cpp
+++ src/image_crop_client_node.cpp
@@ -1,43 +1,92 @@
 #include <chrono>
 #include <memory>
+#include <sstream>
+#include <filesystem> 
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include<unistd.h> 
 #include "rclcpp/rclcpp.hpp" 
 #include "image_processing_pkg/msg/target_cropped_axis.hpp"
+#include "image_processing_pkg/srv/image_crop.hpp"
 
 using namespace std;
-using namespace std::chrono_literals;
+using namespace std::chrono;
+
+using std::placeholders::_1;
+using std::placeholders::_2;
 
 class ImageCropClient : public rclcpp::Node
 {
   public:
-    ImageCropClient()
-    : Node("image_crop_client_node")
-    {
-      publisher_ = this->create_publisher<image_processing_pkg::msg::TargetCroppedAxis>("target_cropped_axis", 10);
-      timer_ = this->create_wall_timer(
-        1000ms, std::bind(&ImageCropClient::timer_callback, this));
-    }
-
-  private:
     void timer_callback()
     {
-
       auto message = image_processing_pkg::msg::TargetCroppedAxis();
       message.x = 0.5;
       message.y = 0.5;
       RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.x << "' and '" << message.y);
-
-      publisher_->publish(message);
-    }
-
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<image_processing_pkg::msg::TargetCroppedAxis>::SharedPtr publisher_;
-  
+      //publisher->publish(message);
+    }  
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ImageCropClient>());
+ 
+  auto node = rclcpp::Node::make_shared("image_crop_client_node");
+  auto client = node->create_client<image_processing_pkg::srv::ImageCrop>("process_image");
+  auto publisher = node->create_publisher<image_processing_pkg::msg::TargetCroppedAxis>("target_cropped_axis", 10);
+  string root_path = std::filesystem::current_path(); 
+  node->declare_parameter("tries", 3);
+  node->declare_parameter("cropped_image_path", root_path + "/src/image_processing_pkg/src/cropped/");
+  auto request = std::make_shared<image_processing_pkg::srv::ImageCrop::Request>();
+  
+  string cropped_image_path = node->get_parameter("cropped_image_path").as_string();
+  int tries = node->get_parameter("tries").as_int();
+
+  while (!client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(node->get_logger(), "service not available, waiting again...");
+  }
+  for (int i = 1;i<=tries;i++){
+    request->image_path = "/home/hygor/ros2_ws/src/image_processing_pkg/src/images/image_1.png";
+    request->request_id = i;
+    RCLCPP_INFO(node->get_logger(), "Request '%d'- Image : '%s'", i, request->image_path.c_str());    
+    auto target_cropped_axis = image_processing_pkg::msg::TargetCroppedAxis();
+    target_cropped_axis.x = i * 0.25;
+    target_cropped_axis.y = i * 0.25;
+    publisher->publish(target_cropped_axis);
+    sleep(0.5);
+    auto result = client->async_send_request(request);
+
+    if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS)
+    {
+      RCLCPP_INFO(node->get_logger(), "Image crop complete");
+      RCLCPP_INFO(node->get_logger(), "Starting save proccess...");
+      sensor_msgs::msg::Image image = result.get()->image;
+      //Save the cropped Image
+      stringstream ss;
+      string folderCreateCommand = "mkdir -p " + cropped_image_path;
+      system(folderCreateCommand.c_str());
+      milliseconds ms = duration_cast< milliseconds >(
+      system_clock::now().time_since_epoch()
+      );
+      ss<<cropped_image_path<<"/"<<"cropped_image_"<<i<<"_"<<ms.count()<<".jpg";
+      string fullPath = ss.str();
+      ss.str("");
+      
+      cv_bridge::CvImagePtr cvImage = cv_bridge::toCvCopy(image);
+      cv::imwrite(fullPath, cvImage->image);
+      RCLCPP_INFO(node->get_logger(), "Image: '%s' Saved!", fullPath.c_str());
+      sleep(1);
+    } else {
+      RCLCPP_ERROR(node->get_logger(), "Failed to call service image_crop_server_node");
+    }
+  }  
+
+  //rclcpp::spin(std::make_shared<ImageCropClient>());
   rclcpp::shutdown();
   return 0;
 }
--- src/image_crop_server_node.cpp
+++ src/image_crop_server_node.cpp
@@ -19,46 +19,37 @@ using std::placeholders::_2;
 
 class ImageCropServer : public rclcpp::Node
 {    
-    
-    public:
+    float x_axis= 0; 
+    float y_axis = 0;
+    public:    
     ImageCropServer()
     : Node("image_crop_server_node")
     {   
         string root_path = std::filesystem::current_path();    
         RCLCPP_INFO(this->get_logger(), "The directory path is: '%s'", root_path.c_str());
         this->declare_parameter("image_path","");
-        this->declare_parameter("tries", 1);
-        this->declare_parameter("cropped_image_path", "");
         subscription_ = this->create_subscription<image_processing_pkg::msg::TargetCroppedAxis>(
         "target_cropped_axis", 10, std::bind(&ImageCropServer::topic_callback, this, _1));
-        service_ = this->create_service<image_processing_pkg::srv::ImageCrop>("process_image", std::bind(&ImageCropServer::process_image, this, _1, _2));        
+        service_ = this->create_service<image_processing_pkg::srv::ImageCrop>("process_image", std::bind(&ImageCropServer::process_image, this, _1, _2));
     }
 
-    void topic_callback(const image_processing_pkg::msg::TargetCroppedAxis & msg) const
+    void topic_callback(const image_processing_pkg::msg::TargetCroppedAxis & msg)
     {
       RCLCPP_INFO(this->get_logger(), "I heard: '%f' and '%f'", msg.x, msg.y);
+      x_axis= msg.x;
+      y_axis = msg.y;
     }
 
     void process_image(const image_processing_pkg::srv::ImageCrop::Request::SharedPtr request, 
     image_processing_pkg::srv::ImageCrop::Response::SharedPtr response)
     {
-        string cropped_path = request->cropped_image_path;
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %s",cropped_path.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request id '%d' Image '%s'",request->request_id, request->image_path.c_str());
 
         string root_path = std::filesystem::current_path(); 
         string image_path = this->get_parameter("image_path").as_string();
-        int tries = this->get_parameter("tries").as_int();
         if(image_path.empty()){
             image_path = root_path + "/src/image_processing_pkg/src/images/image_1.png";
-        }
-
-        if(cropped_path.empty()){
-            cropped_path = root_path + "/src/image_processing_pkg/src/cropped";
-        }
-
-        if(tries < 1){
-            tries = 1;
-        }        
+        }      
 
         RCLCPP_INFO(this->get_logger(), "The path is: '%s'", image_path.c_str());
 
@@ -68,39 +59,34 @@ class ImageCropServer : public rclcpp::Node
 
         
         // Crop image parameters (crop image always from the center point)
-        int offset_x = img.size().width/2;
-        int offset_y = img.size().height/2;
-        int cropped_width = img.size().width * 0.5;
-        int cropped_height = img.size().height * 0.5;
-        int final_offset_x = std::max(0, offset_x - cropped_width/2);
-        int final_offset_y = std::max(0, offset_y - cropped_height/2);
+        if(x_axis > 0 && y_axis > 0){
+            int offset_x = img.size().width/2;
+            int offset_y = img.size().height/2;
+            int cropped_width = img.size().width * x_axis;
+            int cropped_height = img.size().height * y_axis;
+            int final_offset_x = std::max(0, offset_x - cropped_width/2);
+            int final_offset_y = std::max(0, offset_y - cropped_height/2);
 
-        RCLCPP_INFO(this->get_logger(), "Offset X: '%s'", std::to_string(final_offset_x).c_str());
-        RCLCPP_INFO(this->get_logger(), "Offset Y: '%s'", std::to_string(final_offset_y).c_str());
-        RCLCPP_INFO(this->get_logger(), "Height to crop: '%s'", std::to_string(cropped_height).c_str());
-        RCLCPP_INFO(this->get_logger(), "Width to crop: '%s'", std::to_string(cropped_width).c_str());
+            RCLCPP_INFO(this->get_logger(), "Offset X: '%s'", std::to_string(final_offset_x).c_str());
+            RCLCPP_INFO(this->get_logger(), "Offset Y: '%s'", std::to_string(final_offset_y).c_str());
+            RCLCPP_INFO(this->get_logger(), "Height to crop: '%s'", std::to_string(cropped_height).c_str());
+            RCLCPP_INFO(this->get_logger(), "Width to crop: '%s'", std::to_string(cropped_width).c_str());
 
-        cv::Rect r(final_offset_x,final_offset_y,cropped_width,cropped_height);
-        cv::Mat cropped_image = img(r);
+            cv::Rect r(final_offset_x,final_offset_y,cropped_width,cropped_height);
+            cv::Mat cropped_image = img(r);
+            
+            //display image (debug)
+            //cv::imshow("Original Image", img);        
+            //cv::imshow("Cropped Image", cropped_image);            
+            msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", cropped_image).toImageMsg();
+            
+            response->image = *msg_.get();
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Response sent: Finishing process");
+            //cv::waitKey(0);
+        }
         
-        //display image
-        cv::imshow("Original Image", img);        
-        cv::imshow("Cropped Image", cropped_image);
         
-        //Save the cropped Image
-        stringstream ss;
-        string folderCreateCommand = "mkdir " + cropped_path;
-        system(folderCreateCommand.c_str());
-        ss<<cropped_path<<"/"<<"cropped_image_"<<tries<<".jpg";
-        string fullPath = ss.str();
-        ss.str("");
-        cv::imwrite(fullPath, cropped_image);
-
-        msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", cropped_image).toImageMsg();
-        
-        response->image = *msg_.get();
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Response sent: Finishing process");
-        cv::waitKey(0);        
+                
     }
 
     private:        
